acadc89bf36535632ecfbb434c6528c05a5a1d30
MRredstone667 <andy.vyhlidal@gmail.com>
2025-12-13 09:54:03 +0100

    Auto update - 13.12.2025  9:54:02,94

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,47 @@
 import UIKit
+import AVKit
 import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+  private var pipController: AVPictureInPictureController?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let controller = window?.rootViewController as! FlutterViewController
+    let pipChannel = FlutterMethodChannel(name: "com.yourapp.pip",
+                                          binaryMessenger: controller.binaryMessenger)
+
+    pipChannel.setMethodCallHandler { call, result in
+      if call.method == "playVideoInPiP",
+         let args = call.arguments as? [String: String],
+         let path = args["path"] {
+        self.startPiPVideo(path: path)
+        result(nil)
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    }
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func startPiPVideo(path: String) {
+    guard let url = URL(string: path) ?? URL(fileURLWithPath: path) else { return }
+
+    let player = AVPlayer(url: url)
+    let vc = AVPlayerViewController()
+    vc.player = player
+    vc.allowsPictureInPicturePlayback = true
+    vc.player?.play()
+
+    if AVPictureInPictureController.isPictureInPictureSupported(),
+       let layer = vc.playerLayer {
+      pipController = AVPictureInPictureController(playerLayer: layer)
+      pipController?.startPictureInPicture()
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,27 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:pip_view/pip_view.dart';
-import 'package:pip_view_app_demo/background.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+const channel = MethodChannel('com.yourapp.pip');
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+class HomePage extends StatelessWidget {
+  // výběr videa
+  Future<void> pickVideo() async {
+    final result = await FilePicker.platform.pickFiles(type: FileType.video);
+    if (result != null) {
+      final path = result.files.single.path!;
+      await channel.invokeMethod('playVideoInPiP', {'path': path});
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return PIPView(
-      builder: (context, isFloating) => Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
-          ),
-        ),
-       floatingActionButton: isFloating
-    ? null // 👈 Když je PiP zapnutý, tlačítko se nezobrazí
-    : FloatingActionButton(
-        onPressed: () {
-          PIPView.of(context)?.presentBelow(const BackGroundScreen());
-        },
-        tooltip: 'Show PiP',
-        child: const Icon(Icons.picture_in_picture),
-      ),
+    return Scaffold(
+      body: Center(child: Text('Vyber video a pusť do PiP')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: pickVideo,
+        child: Icon(Icons.picture_in_picture),
       ),
     );
   }
